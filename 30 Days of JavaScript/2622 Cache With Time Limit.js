20415baad82d4e7e85dd91a67bf8e637ace1e3b4
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-22 18:55:13 +0530

    Create 2622 Cache With Time Limit.js

---
+++ 30 Days of JavaScript/2622 Cache With Time Limit.js
@@ -0,0 +1,50 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let result = false;
+    if (this.cache.has(key)) {
+        result = true;
+        const val = this.cache.get(key);
+        clearTimeout(val[1]);
+        this.cache.delete(key);
+    }
+    const timerId = setTimeout(() => {
+        this.cache.delete(key);
+    }, duration);
+    this.cache.set(key, [value, timerId]);
+    return result;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    if (this.cache.has(key)) {
+        return this.cache.get(key)[0];
+    }
+    return -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
